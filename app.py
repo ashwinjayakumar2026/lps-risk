4540ff130dcec8df907158fa10afdb83ed1dd91d
ashwinjayakumar2026 <ashwinjayakumar2026@gmail.com>
2026-04-19 15:32:46 +0530

    Update app.py

--- app.py
+++ app.py
@@ -1,5 +1,4 @@
-import folium
-from streamlit_folium import st_folium
+
 import pandas as pd
 import json
 
@@ -9,16 +8,17 @@ with open("districts_light.geojson") as f:
 
 df = pd.read_csv("final_app_data.csv")
 
-# Handle NaN (important for visualization)
-gdf[col] = gdf[col].fillna(0)
+import folium
+from streamlit_folium import st_folium
 
-# Create map
+# Fill NaN
+df[col] = df[col].fillna(0)
+
 m = folium.Map(location=[22, 80], zoom_start=4, tiles="cartodbpositron")
 
-# Choropleth
 folium.Choropleth(
     geo_data=geojson,
-    data=gdf,
+    data=df,
     columns=["DIST_ID", col],
     key_on="feature.properties.DIST_ID",
     fill_color="YlOrRd",
@@ -27,15 +27,4 @@ folium.Choropleth(
     legend_name=col
 ).add_to(m)
 
-# 🔥 ADD TOOLTIP (IMPORTANT)
-folium.GeoJson(
-    geojson,
-    tooltip=folium.GeoJsonTooltip(
-        fields=["district_clean"],
-        aliases=["District:"],
-        localize=True
-    )
-).add_to(m)
-
-# Render
 st_folium(m, width=700, height=500)
